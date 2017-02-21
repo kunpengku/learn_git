50dd1b65eb3ba4701fc25a8789687438c22c6a64
fupeng <fupeng@okjiaoyu.cn>
2017-02-21 11:32:34 +0800

    归并排序  快速排序  这两种是 nlogn的 复杂度

---
+++ src/main/java/learn_sort/QuickSort.java
@@ -0,0 +1,62 @@
+package learn_sort;
+
+/**
+ * Created by yuan on 2017/2/18.
+ * 快速排序 ，因为真的很快
+ * 尤其是面对随机的数组时候，
+ * 因此，数组规模越大，越应该用快速排序，
+ * 如果数组规模很小，则可以是用选择排序。
+ *
+ * 春夏秋冬，进入最好在冬天，离开一定在夏天。
+ */
+
+
+
+public class QuickSort {
+
+    private static void show(int [] src){
+        for(int i : src){
+            System.out.println(i);
+        }
+    }
+
+    private static void exch(int [] src, int i, int j){
+        int tmp = src[i];
+        src[i] = src[j];
+        src[j] = tmp;
+    }
+
+    static int partition(int [] src, int lo, int hi){
+        int key = src[lo];
+        int i=lo;
+        int j=hi;
+        while(i!=j){
+            while(src[j] >= key && i<j)
+                j--;
+            while(src[i] <= key && i<j)
+                i++;
+            if(i!=j)
+                exch(src,i,j);
+        }
+        exch(src,i,lo);
+
+        return i;
+    }
+
+    static void quick_sort(int [] src, int lo, int hi){
+        if(lo>=hi)
+            return;
+        int k = partition(src,lo,hi);
+
+        quick_sort(src,lo, k-1);
+        quick_sort(src,k+1,hi);
+
+    }
+    public static void main(String[] args) {
+        int [] src = {9,7,5,3,1,2,4,6,8,0};
+
+        quick_sort(src,0, src.length-1);
+
+        show(src);
+    }
+}
